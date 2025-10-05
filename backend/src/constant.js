0dfb0d61348488bbc162a2d9c524c9b3c2d740de
Kavya Gupta <kavyagupta0605@gmail.com>
2025-10-06 01:18:05 +0530

    database connected

---
+++ backend/src/constant.js
@@ -0,0 +1,1 @@
+export const DB_NAME = "Traffic-Data-Visualizer";
---
+++ backend/src/server.js
@@ -0,0 +1,19 @@
+import dotenv from "dotenv";
+import connectDB from "./db/dbconnect.js";
+import { app } from "./app.js";
+dotenv.config({
+  path: "../env",
+});
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("Error : ", error);
+      throw error;
+    });
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port : ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("Mongo db connection failed", error);
+  });
